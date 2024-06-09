bec510de920277022ea04486019e34cafd44fb3b
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-06-09 11:18:15 -0700

    found a working example for calling the auth and token api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import {getToken} from './api/apiToken';
+import React, {useState, useEffect} from 'react'
+import {code, redirectToAuthCodeFlow, getAccessToken, fetchProfile, clientId} from './api/apiToken';
+import {} from './api/apiToken';
 import Searchbar from './components/searchbar';
 import './App.css';
 
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+  const accessToken = await getAccessToken(clientId, code);
+  const profile = await fetchProfile(accessToken);
+  console.log(profile)
+}
+
 function App() {
-
-  const accessToken = getToken;
-
   return (
     <div className="App">
       <header className="App-header">
+
         <Searchbar />
       </header>
     </div>
--- src/api/apiToken.js
+++ src/api/apiToken.js
@@ -1,54 +1,84 @@
-const clientId = process.env.CLIENT_ID; 
-const redirectUrl = 'http://localhost:3000';
-const tokenEndpoint = "https://accounts.spotify.com/api/token"
-const scope = 'user-read-private user-read-email';
-const authUrl = new URL("https://accounts.spotify.com/authorize");
+export const clientId = '63f74899ca954c22aa8844c796fdfbbc';
+const redirect_uri = 'http://localhost:3000';
+const params = new URLSearchParams(window.location.search);
+export const code = params.get("code");
 
-const generateRandomString = (length) => {
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const values = crypto.getRandomValues(new Uint8Array(length));
-    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+function generateCodeVerifier(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
 
-const codeVerifier  = generateRandomString(64);
-
-const sha256 = async (plain) => {
-    const encoder = new TextEncoder()
-    const data = encoder.encode(plain)
-    return window.crypto.subtle.digest('SHA-256', data)
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
 }
-  
-const base64encode = (input) => {
-    return btoa(String.fromCharCode(...new Uint8Array(input)))
-      .replace(/=/g, '')
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_');
-}
-  
-const urlParams = new URLSearchParams(window.location.search);
-let code = urlParams.get('code');
-  
 
-export async function getToken(code) {
-    const code_verifier = localStorage.getItem('code_verifier');
-  
-    const response = await fetch(tokenEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        client_id: clientId,
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirectUrl,
-        code_verifier: code_verifier,
-      }),
+export async function redirectToAuthCodeFlow(clientId) {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", redirect_uri);
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+export async function getAccessToken(clientId, code) {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", redirect_uri);
+    params.append("code_verifier", verifier);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
     });
-  
-    return await response.json();
-  }
 
-  console.log(getToken(code))
+    const { access_token } = await result.json();
+    return access_token;
+}
 
-  
+export async function fetchProfile(token) {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
+
+export function populateUI(profile) {
+    document.getElementById("displayName").innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar").appendChild(profileImage);
+        document.getElementById("imgUrl").innerText = profile.images[0].url;
+    }
+    document.getElementById("id").innerText = profile.id;
+    document.getElementById("email").innerText = profile.email;
+    document.getElementById("uri").innerText = profile.uri;
+    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url").innerText = profile.href;
+    document.getElementById("url").setAttribute("href", profile.href);
+}
